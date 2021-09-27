4a331439af2e1520d25875753bcbb60670dd29a4
Ukeachusim Loveth Ifunanya <ukeloveth247@gmail.com>
2021-09-27 01:34:31 +0100

    .

---
+++ js/dom-event-task.js
@@ -0,0 +1,22 @@
+//alert("very good")
+// No 1. insert only the essential script to gap for the document Object model to FontFaceSetLoadEvent,
+// to make sure that anything you manipulate in the document Object model has loaded. Try to achieve this by adding an event listener or any other methods you PerformanceResourceTiming.
+
+//No 2. Replace the text "Change me " with "Hello World"
+// No3. When a user hovers over one of the colored boxes change the text to display the color that is being hovered over
+//No4. Create a new div element
+//No 5. Give your new div a class of purple and style it so that it has a background color of purple
+//No 6. Append your new div to the page to the section tag
+
+//Part 2
+//Develop a game (RACING) with the three Cars.When the race button is pressed,the three cars should move the screen untill one 
+//of them is at the screen.When one of the blocks reaches the end - you should alert "Winner!"
+
+//No4. Create a new div element
+let elem = document.createElement("div")
+//No 5. Give your new div a class of purple and style it so that it has a background color of purple
+elem.add.class="purple"
+elem.style.backgroundColor ="purple"
+//No 6. Append your new div to the page to the section tag
+let info = document.getElementsByTagName("section")
+info.appendChild(elem)
